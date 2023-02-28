23dfe8570f7513709fb795af3445ce7742da3538
johnhem <johnherma72b@gmail.com>
2023-02-28 09:35:08 -0800

    This is the first task of pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * In this task our main objective is  function that takes a pointer to an int as parameter and updates the value it points to to 98. 
+ *
+ * Return: value is 0;
+ */
+void reset_to_98(int *n){
+	*n = 98;
+	return 0;
+}
